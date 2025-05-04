cabebc2df5f091c0ba229f1348e341f4c58765d8
hj-core <hj.forbetterfuture@gmail.com>
2025-05-04 21:24:36 +0800

    os: Clear the bss during kernel initialization

--- os/src/main.rs
+++ os/src/main.rs
@@ -9,5 +9,16 @@ global_asm!(include_str!("entry.asm"));
 
 #[unsafe(no_mangle)]
 pub fn rust_main() -> ! {
+    clear_bss();
     loop {}
+}
+
+fn clear_bss() {
+    unsafe extern "C" {
+        unsafe fn bss_start();
+        unsafe fn bss_end();
+    }
+    (bss_start as usize..bss_end as usize).for_each(|a| {
+        unsafe { (a as *mut u8).write_volatile(0) };
+    });
 }
