5159dd92cb800dbe6025e21d23cd7af2472236c5
hj-core <hj.forbetterfuture@gmail.com>
2025-05-05 19:41:15 +0800

    user: Add a new app 04_privilegecsr

---
+++ user/src/bin/04_privilegecsr.rs
@@ -0,0 +1,17 @@
+#![no_std]
+#![no_main]
+
+extern crate user_lib;
+
+use core::arch::asm;
+
+use user_lib::println;
+
+#[unsafe(no_mangle)]
+fn main() -> i32 {
+    println!("Try to access privileged CSR in U Mode");
+    println!("Kernel should kill this application!");
+
+    unsafe { asm!("csrr {}, sstatus", out(reg) _) };
+    0
+}
