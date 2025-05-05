d03f71385b1002cccce8b72fec757e635c9e65c9
hj-core <hj.forbetterfuture@gmail.com>
2025-05-05 18:11:38 +0800

    user::lang_items: Evolve the panic_handler
    The panic_handler now prints the panic information and then exit the
    process.

--- user/src/lang_items.rs
+++ user/src/lang_items.rs
@@ -1,4 +1,17 @@
+use crate::{println, syscall::sys_exit};
+
 #[panic_handler]
-fn panic(_info: &core::panic::PanicInfo) -> ! {
-    loop {}
+fn panic(info: &core::panic::PanicInfo) -> ! {
+    if let Some(location) = info.location() {
+        println!(
+            "Panicked at {}:{} {}",
+            location.file(),
+            location.line(),
+            info.message()
+        );
+    } else {
+        println!("Panicked: {}", info.message());
+    }
+    sys_exit(-1);
+    unreachable!()
 }
