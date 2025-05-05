cc7989a841e27ab8818092210da45205ad886b23
hj-core <hj.forbetterfuture@gmail.com>
2025-05-05 18:51:46 +0800

    user: Add a new app 01_storefault

---
+++ user/src/bin/01_storefault.rs
@@ -0,0 +1,15 @@
+#![no_std]
+#![no_main]
+
+extern crate user_lib;
+
+use user_lib::println;
+
+#[unsafe(no_mangle)]
+fn main() -> i32 {
+    println!("Into Test store_fault, we will insert an invalid store operation...");
+    println!("Kernel should kill this application!");
+
+    unsafe { core::ptr::null_mut::<u8>().write_volatile(0) };
+    0
+}
