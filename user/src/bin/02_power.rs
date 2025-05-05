051f7cbc083840ad1c460e88fa8c8c770ad4cc25
hj-core <hj.forbetterfuture@gmail.com>
2025-05-05 19:05:40 +0800

    user: Add a new app 02_power

---
+++ user/src/bin/02_power.rs
@@ -0,0 +1,32 @@
+#![no_std]
+#![no_main]
+
+extern crate user_lib;
+
+use user_lib::println;
+
+#[unsafe(no_mangle)]
+fn main() -> i32 {
+    let mut buffer = [0u32; 10];
+    let buffer_size = buffer.len();
+
+    const BASE: u32 = 3;
+    const POW_CEIL: u32 = 100_000;
+    const MODULE: u32 = 10_007;
+
+    let mut index = 0;
+    buffer[index] = 1;
+
+    for pow in 1..=POW_CEIL {
+        let prev_value = buffer[index];
+        let value = (prev_value * BASE) % MODULE;
+        index = (index + 1) % buffer_size;
+        buffer[index] = value;
+
+        if pow % 10_000 == 0 {
+            println!("{}^{}={}(MOD {})", BASE, pow, value, MODULE);
+        }
+    }
+    println!("Test power OK!");
+    0
+}
