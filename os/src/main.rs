5e8399a0ec690f615ffcc7b5388f19616fb664c9
hj-core <hj.forbetterfuture@gmail.com>
2025-05-11 12:37:15 +0800

    os::main: Remove test_riscv_macros

--- os/src/main.rs
+++ os/src/main.rs
@@ -24,8 +24,6 @@ pub fn rust_main() -> ! {
     log_kernel_layout();
     log_apps_layout();
 
-    test_riscv_macros();
-
     trap::init();
     unsafe { asm!("li t0, 0x00000020", "jalr x0, t0, 0",) };
 
@@ -104,85 +102,4 @@ fn log_apps_layout() {
         let size = app_end - app_start;
         debug!("app_{} [{:#x}, {:#x}) size={}", i, app_start, app_end, size);
     }
-}
-
-fn test_riscv_macros() {
-    test_riscv_csrr();
-    test_riscv_csrw();
-    test_riscv_csrrc();
-    test_riscv_csrrs();
-}
-
-fn test_riscv_csrr() {
-    const STVAL_NO: usize = 0x143;
-    let mut stval_val: usize;
-
-    unsafe { asm!("csrw {csr}, {rd}", csr = const STVAL_NO, rd = in(reg) 20) };
-    riscv::csrr!(STVAL_NO, stval_val);
-    assert_eq!(20, stval_val);
-
-    unsafe { asm!("csrw {csr}, {rd}", csr = const STVAL_NO,rd = in(reg) 55) };
-    riscv::csrr!(STVAL_NO, stval_val);
-    assert_eq!(55, stval_val);
-
-    debug!("riscv::csrr worked correctly");
-}
-
-fn test_riscv_csrw() {
-    const STVAL_NO: usize = 0x143;
-    let mut stval_val: usize;
-
-    riscv::csrw!(STVAL_NO, 256);
-    unsafe { asm!("csrr {rd}, {csr}", rd = lateout(reg) stval_val, csr = const STVAL_NO) };
-    assert_eq!(256, stval_val);
-
-    riscv::csrw!(STVAL_NO, 996);
-    unsafe { asm!("csrr {rd}, {csr}", rd = lateout(reg) stval_val, csr = const STVAL_NO) };
-    assert_eq!(996, stval_val);
-
-    debug!("riscv::csrw worked correctly");
-}
-
-fn test_riscv_csrrc() {
-    const STVAL_NO: usize = 0x143;
-    let mut old_stval_val: usize;
-    let mut new_stval_val: usize;
-
-    // Clear a single set bit
-    unsafe { asm!("csrw {csr}, {rd}", csr = const STVAL_NO, rd = in(reg) 0b1111) };
-    riscv::csrrc!(STVAL_NO, old_stval_val, 0b1);
-    assert_eq!(0b1111, old_stval_val);
-    unsafe { asm!("csrr {rd}, {csr}", rd = lateout(reg) new_stval_val, csr = const STVAL_NO) };
-    assert_eq!(0b1110, new_stval_val);
-
-    // Clear multiple bits including set bits and an unset bit
-    unsafe { asm!("csrw {csr}, {rd}", csr = const STVAL_NO, rd = in(reg) 0b01111) };
-    riscv::csrrc!(STVAL_NO, old_stval_val, 0b11010);
-    assert_eq!(0b01111, old_stval_val);
-    unsafe { asm!("csrr {rd}, {csr}", rd = lateout(reg) new_stval_val, csr = const STVAL_NO) };
-    assert_eq!(0b0101, new_stval_val);
-
-    debug!("riscv::csrrc worked correctly");
-}
-
-fn test_riscv_csrrs() {
-    const STVAL_NO: usize = 0x143;
-    let mut old_stval_val: usize;
-    let mut new_stval_val: usize;
-
-    // Set a single unset bit
-    unsafe { asm!("csrw {csr}, {rd}", csr = const STVAL_NO, rd= in(reg) 0b1001) };
-    riscv::csrrs!(STVAL_NO, old_stval_val, 0b10);
-    assert_eq!(0b1001, old_stval_val);
-    unsafe { asm!("csrr {rd}, {csr}", rd = lateout(reg) new_stval_val, csr = const STVAL_NO) };
-    assert_eq!(0b1011, new_stval_val);
-
-    // Set mutliple unset bits and a set bit
-    unsafe { asm!("csrw {csr}, {rd}", csr = const STVAL_NO, rd= in(reg) 0b0100) };
-    riscv::csrrs!(STVAL_NO, old_stval_val, 0b1111);
-    assert_eq!(0b0100, old_stval_val);
-    unsafe { asm!("csrr {rd}, {csr}", rd = lateout(reg) new_stval_val, csr = const STVAL_NO) };
-    assert_eq!(0b1111, new_stval_val);
-
-    debug!("riscv::csrrs worked correctly");
 }
