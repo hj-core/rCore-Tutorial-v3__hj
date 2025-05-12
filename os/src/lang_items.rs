9fe7daf53598aa5148af0bd97a85fa57fcdf0d8b
hj-core <hj.forbetterfuture@gmail.com>
2025-05-12 23:02:13 +0800

    os::lang_items: Enhance panic handler with print_stack_trace

--- os/src/lang_items.rs
+++ os/src/lang_items.rs
@@ -1,3 +1,5 @@
+use core::arch::asm;
+
 use crate::{println, sbi::shutdown};
 
 #[panic_handler]
@@ -12,5 +14,23 @@ fn panic(info: &core::panic::PanicInfo) -> ! {
     } else {
         println!("Panicked: {}", info.message());
     }
+    unsafe { print_stack_trace() };
     shutdown(true)
+}
+
+/// Prints a stack trace of the current execution.
+///
+/// # Safety
+/// This function relies on the saved frame pointers to work correctly.
+unsafe fn print_stack_trace() {
+    println!("#------- Stack Trace (most recent first) -------#");
+    let mut fp: *const usize;
+
+    unsafe { asm!("mv {}, fp", lateout(reg) fp) };
+    while !fp.is_null() {
+        let ra = unsafe { fp.offset(-1).read() };
+        fp = unsafe { fp.offset(-2).read() as *const usize };
+        println!("| fp {:#018x}, ra {:#018x}  |", fp as usize, ra);
+    }
+    println!("#-----------------------------------------------#");
 }
