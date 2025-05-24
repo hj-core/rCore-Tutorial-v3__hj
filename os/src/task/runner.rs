4a5f0fab1985c7c2c82387c7f9ffe25ebf551e70
hj-core <hj.forbetterfuture@gmail.com>
2025-05-24 14:19:18 +0800

    os::task::runner: Find next app by TaskState

--- os/src/task/runner.rs
+++ os/src/task/runner.rs
@@ -6,11 +6,14 @@ use core::{
 use crate::{
     debug, info, log,
     sbi::shutdown,
-    task::{TASK_CONTROL_BLOCK, control::TaskState, debug_print_tcb},
+    task::{
+        KernelStack, TASK_CONTROL_BLOCK,
+        control::TaskState,
+        debug_print_tcb,
+        loader::{get_app_name, get_total_apps},
+    },
     trap::{self, TrapContext},
 };
-
-use super::{KernelStack, loader};
 
 static CURRENT_APP_INDEX: AtomicUsize = AtomicUsize::new(0);
 
@@ -27,29 +30,46 @@ pub(super) fn run_first_app() -> ! {
 }
 
 pub(crate) fn run_next_app() -> ! {
-    let app_index = CURRENT_APP_INDEX.fetch_add(1, Ordering::Relaxed) + 1;
-    if app_index == loader::get_total_apps() {
+    if let Some(app_index) = find_next_app() {
+        run_app(app_index)
+    } else {
         debug_print_tcb();
         info!("No more apps to run, bye bye.");
         shutdown(false)
     }
-    run_app(app_index)
+}
+
+fn find_next_app() -> Option<usize> {
+    let curr_index = get_current_app_index();
+    let total_apps = get_total_apps();
+
+    ((curr_index + 1)..(curr_index + 1 + total_apps))
+        .map(|i| if i < total_apps { i } else { i - total_apps })
+        .find(|&i| matches!(TASK_CONTROL_BLOCK[i].lock().get_state(), TaskState::Ready))
 }
 
 fn run_app(app_index: usize) -> ! {
     assert!(
-        app_index < loader::get_total_apps(),
+        app_index < get_total_apps(),
         "Invalid app index {app_index}"
+    );
+
+    assert_eq!(
+        TASK_CONTROL_BLOCK[app_index].lock().get_state(),
+        TaskState::Ready,
+        "Cannot run a non-ready app"
     );
 
     TASK_CONTROL_BLOCK[app_index]
         .lock()
         .change_state(TaskState::Running);
 
+    CURRENT_APP_INDEX.store(app_index, Ordering::Relaxed);
+
     let time = read_system_time_ms();
     debug!(
         "{} starts at {}.{:03} seconds since system start",
-        loader::get_app_name(app_index),
+        get_app_name(app_index),
         time / 1000,
         time % 1000,
     );
